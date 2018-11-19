473a9147612048aa52ebdb7d3d56118ecca9465f
Osio Alberto <osioalberto@gmail.com>
2018-11-19 13:48:31 +0100

    Added persistency

--- src/Root.js
+++ src/Root.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import App from './App';
+import { PersistGate } from 'redux-persist/integration/react';
 
-const Root = ({ store }) => (
+const Root = ({ store, persistor }) => (
     <Provider store={store}>
-        <Router>
-            <Route path="/" component={App} />
-        </Router>
+        <PersistGate loading={null} persistor={persistor}>
+            <Router>
+                <Route path="/" component={App} />
+            </Router>
+        </PersistGate>
     </Provider>
 );
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { createStore, applyMiddleware } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
+import logger from 'redux-logger';
+import { persistReducer, persistStore } from 'redux-persist';
+import hardSet from 'redux-persist/es/stateReconciler/hardSet';
+import storage from 'redux-persist/lib/storage';
 import RootReducer from './reducers/RootReducer';
 import Root from './Root';
-import logger from 'redux-logger';
 import './style/index.scss';
 
-const store = createStore(RootReducer, applyMiddleware(logger));
-render(<Root store={store} />, document.getElementById('root'));
+const persistConfig = {
+    key: 'protocode-root',
+    storage,
+    stateReconciler: hardSet
+}
+
+const persistedReducer = persistReducer(persistConfig, RootReducer);
+
+const store = createStore(persistedReducer, applyMiddleware(logger));
+let persistor = persistStore(store);
+render(
+    <Root store={store} persistor={persistor} />,
+    document.getElementById('root')
+);
